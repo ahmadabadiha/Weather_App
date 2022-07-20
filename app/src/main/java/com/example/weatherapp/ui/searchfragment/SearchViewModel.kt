538322f0e139fd063b23a5e3f6ea79fa8e5d4ab8
Ali Ahmadabadiha <Ahmadabadiha@gmail.com>
2022-07-20 17:59:38 +0430

    search fragment class and its view model created

---
+++ app/src/main/java/com/example/weatherapp/ui/searchfragment/SearchViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.weatherapp.ui.searchfragment
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SearchViewModel: ViewModel() {
+
+}
