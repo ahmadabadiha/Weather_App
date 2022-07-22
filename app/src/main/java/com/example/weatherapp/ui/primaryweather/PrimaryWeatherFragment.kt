32d8681242e8c2fd96d66b56687e47b13eeaa109
Ali Ahmadabadiha <Ahmadabadiha@gmail.com>
2022-07-22 16:50:23 +0430

    showing weekly forecast implemented

--- app/src/main/java/com/example/weatherapp/ui/primaryweather/PrimaryWeatherFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/primaryweather/PrimaryWeatherFragment.kt
@@ -8,7 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import com.example.weatherapp.R
-import com.example.weatherapp.data.model.WeatherResponse
+import com.example.weatherapp.data.model.Current
 import com.example.weatherapp.data.remote.ResultWrapper
 import com.example.weatherapp.databinding.FragmentPrimaryWeatherBinding
 import com.example.weatherapp.ui.collectFlow
@@ -19,20 +19,26 @@ class PrimaryWeatherFragment : Fragment(R.layout.fragment_primary_weather) {
     private var _binding: FragmentPrimaryWeatherBinding? = null
     private val binding get() = _binding!!
     private val viewModel: PrimaryWeatherViewModel by viewModels()
+    private lateinit var weeklyForecastRecyclerAdapter: WeeklyForecastRecyclerAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentPrimaryWeatherBinding.bind(view)
         binding.cityName.text = viewModel.cityName
+        weeklyForecastRecyclerAdapter = WeeklyForecastRecyclerAdapter { }
+        binding.recyclerView.adapter = weeklyForecastRecyclerAdapter
         collectFlow(viewModel.weatherResults) {
             when (it) {
                 ResultWrapper.Loading -> {
                     binding.loadingAnim.isGone = false
                     binding.loadingAnim.playAnimation()
+                    binding.currentWeatherView.isGone = true
                 }
                 is ResultWrapper.Success -> {
                     binding.loadingAnim.isGone = true
                     binding.loadingAnim.pauseAnimation()
-                    setViews(it.value)
+                    binding.currentWeatherView.isGone = false
+                    setViews(it.value.current)
+                    weeklyForecastRecyclerAdapter.submitList(it.value.daily)
                 }
                 is ResultWrapper.Error -> {
                     binding.loadingAnim.isGone = true
@@ -43,17 +49,19 @@ class PrimaryWeatherFragment : Fragment(R.layout.fragment_primary_weather) {
         }
     }
 
-    private fun setViews(weather: WeatherResponse) {
-        binding.temp.text = weather.current.temp.toString() + "°"
-        binding.feelsLike.text = "Feels like " + weather.current.feels_like.toString() + "°"
-        val icon = weather.current.weather[0].icon
+    private fun setViews(currentWeather: Current) {
+        binding.temp.text = currentWeather.temp.toString() + "°"
+        binding.feelsLike.text = "Feels like " + currentWeather.feels_like.toString() + "°"
+        val icon = currentWeather.weather[0].icon
         Glide.with(this).load("http://openweathermap.org/img/wn/$icon@2x.png").into(binding.weatherIcon)
-        val sdf = java.text.SimpleDateFormat("yyyy-MM-dd HH:mm")
-        val weekDay = java.text.SimpleDateFormat("EEEE")
-        val date = java.util.Date(weather.current.dt.toLong() * 1000)
-        binding.weekday.text = weekDay.format(date)
-        binding.date.text = sdf.format(date)
-        binding.weatherDescription.text = weather.current.weather[0].description
+        val date = java.text.SimpleDateFormat("dd MMMM yyyy")
+        val time = java.text.SimpleDateFormat("HH:mm")
+        val weekDay = java.text.SimpleDateFormat("E")
+        val receivedDate = java.util.Date(currentWeather.dt.toLong() * 1000)
+        binding.weekday.text = weekDay.format(receivedDate)
+        binding.date.text = date.format(receivedDate)
+        binding.time.text = time.format(receivedDate)
+        binding.weatherDescription.text = currentWeather.weather[0].description
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/weatherapp/ui/primaryweather/WeeklyForecastRecyclerAdapter.kt
+++ app/src/main/java/com/example/weatherapp/ui/primaryweather/WeeklyForecastRecyclerAdapter.kt
@@ -1,4 +1,59 @@
 package com.example.weatherapp.ui.primaryweather
 
-class WeeklyForecastRecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.weatherapp.data.model.Daily
+import com.example.weatherapp.databinding.WeekdayLayoutBinding
+
+
+class WeeklyForecastRecyclerAdapter(private val onClick: (dailyForecast: Daily) -> Unit) :
+    ListAdapter<Daily, WeeklyForecastRecyclerAdapter.WeekdayViewHolder>(
+        WeeklyForecastDiffCallback()
+    ) {
+
+    inner class WeekdayViewHolder(private val binding: WeekdayLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+
+        fun fill(item: Daily) {
+            binding.apply {
+                val icon = item.weather[0].icon
+                Glide.with(itemView).load("http://openweathermap.org/img/wn/$icon@2x.png").into(weatherIcon)
+                val dayOfMonth = java.text.SimpleDateFormat("dd MMM")
+                val dayOfWeek = java.text.SimpleDateFormat("E")
+                val receivedDate = java.util.Date(item.dt.toLong() * 1000)
+                weekday.text = dayOfWeek.format(receivedDate)
+                date.text = dayOfMonth.format(receivedDate)
+                temp.text = item.temp.day.toString() + "°"
+            }
+            itemView.setOnClickListener { onClick(item) }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeekdayViewHolder =
+        WeekdayViewHolder(
+            WeekdayLayoutBinding.inflate(
+                LayoutInflater
+                    .from(parent.context), parent, false
+            )
+        )
+
+    override fun onBindViewHolder(holder: WeekdayViewHolder, position: Int) {
+        holder.fill(getItem(position))
+    }
+
+}
+
+class WeeklyForecastDiffCallback : DiffUtil.ItemCallback<Daily>() {
+    override fun areItemsTheSame(oldItem: Daily, newItem: Daily): Boolean {
+        return oldItem.dt == newItem.dt
+    }
+
+    override fun areContentsTheSame(oldItem: Daily, newItem: Daily): Boolean {
+        return oldItem == newItem
+    }
 }
